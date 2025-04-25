72743371a7d33be96d3d3d3070d6dbc3e32d5f7d
jpmjunior <junior.linhares@yahoo.com.br>
2025-04-25 20:13:31 -0300

    add h2-database e profile test

--- src/test/java/br/com/pinheiroapps/pibsheeps/service/EventProcessorTest.java
+++ src/test/java/br/com/pinheiroapps/pibsheeps/service/EventProcessorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -13,6 +14,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventProcessorTest {
 
     @MockitoBean
--- src/test/java/br/com/pinheiroapps/pibsheeps/service/MessengerServiceTest.java
+++ src/test/java/br/com/pinheiroapps/pibsheeps/service/MessengerServiceTest.java
@@ -4,10 +4,12 @@ import br.com.pinheiroapps.pibsheeps.domain.dto.MessageDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MessengerServiceTest {
 
     @Autowired
